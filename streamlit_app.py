4353abeaead8c1e908fee351a61b3c5c59e5f9c5
vrahulblr <vrahul.blr@gmail.com>
2024-08-11 09:16:17 +0000

    Helpful comments added

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,21 +17,10 @@ from langchain.document_loaders import UnstructuredHTMLLoader
 from langchain.schema.document import Document
 from langchain.text_splitter import CharacterTextSplitter
 
-# question = st.text_input("Enter a question that you want answered from the filing", type="default")
-
 st.title("Simple app to demonstrate Retrieval Augmented Generation, fondly known as RAG")
 st.markdown("Enter a URL that you want the app to use as context, and the question you want answered")
-# st.subheader("Enter the URL of filing you wish to query")
-# user_input = st.text_input("Enter your Huggingface API KEY,the URL, the question, separated by commas and no spaces", type="default")
-# fetch_button = st.button("Fetch answer")
-# try:
-#     user_input_as_list = user_input.split(",")
-#     API_KEY = user_input_as_list[0]
-#     url = user_input_as_list[1]
-#     question = user_input_as_list[2]
-# except Exception as e:
-#     print("Awaiting user input")
 
+# Here we are collecting user inputs
 with st.form("user_inputs_for_rag_form", clear_on_submit=False):
     API_KEY = st.text_input("Insert your Hugging Face Access Token here")
     url = st.text_input("Insert the URL you would like to provide as context")
@@ -40,45 +29,39 @@ with st.form("user_inputs_for_rag_form", clear_on_submit=False):
 
 title = []
 text = []
-# db = []
+answer_to_print = []
 
-
+#We will use this function later to call the LLM to generate content
 def query(API_URL, headers, payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-
-
-answer_to_print = []
-
+# We scrape the news site to collect the content over there
 if submit:
     article = Article(url)
     article.download()
     article.parse()
     title.append(article.title)
     text.append(article.text)
-    # print("type and length of text", type(text[0]), len(text[0]))
 
 try:
     doc_creator = CharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     document = doc_creator.create_documents(texts = text)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     docs = text_splitter.split_documents(document)
-    # Define the path to the pre-trained model you want to use
+
+    # Indicate which model you wish to use for creating embeddings
     modelPath = "sentence-transformers/all-MiniLM-l6-v2"
-    # Create a dictionary with model configuration options, specifying to use the CPU for computations
+    # Arguements to send alongside the model configs
     model_kwargs = {'device':'cpu'}
-    # Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
     encode_kwargs = {'normalize_embeddings': False}
     # Initialize an instance of HuggingFaceEmbeddings with the specified parameters
     embeddings = HuggingFaceEmbeddings(
-        model_name=modelPath,     # Provide the pre-trained model's path
-        model_kwargs=model_kwargs, # Pass the model configuration options
-        encode_kwargs=encode_kwargs # Pass the encoding options
+        model_name=modelPath,     
+        model_kwargs=model_kwargs, 
+        encode_kwargs=encode_kwargs 
     )
-    print("embeddings done, proceeding to write in Vector DB")
     db = FAISS.from_documents(docs, embeddings)
-    print("written to vectordb")
     searchDocs = db.similarity_search(question)
     answer_to_print.append(searchDocs[0].page_content)
     st.divider()
@@ -89,12 +72,13 @@ except Exception as e:
 
 
 try:
-    # print(type(answer_to_print[0]), len(answer_to_print[0]), answer_to_print)
+    #We will use the Phi3 model to generate an answer to our prompt
+    API_URL = "https://api-inference.huggingface.co/models/microsoft/Phi-3-mini-4k-instruct"
+    #Here we are creating the prompt using a template corresponding to the model we will use
     message = [
         {"role": "system", "content": "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users. Respond back in 1 sentence. Here is the context: " + f"{answer_to_print[0]}" + "Now answer the following question from the user"},
         {"role": "user", "content": f"{question}"},
     ]
-    API_URL = "https://api-inference.huggingface.co/models/microsoft/Phi-3-mini-4k-instruct"
     headers = {"Authorization": f"Bearer {API_KEY}"}
     output = query(API_URL, headers, {"inputs": str(message),"wait_for_model": True})
     st.divider()
